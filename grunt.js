9833c13951d72d47340f27a41f56664f136eb3cf
Todd Wolfson <todd@twolfson.com>
2012-11-01 01:20:41 -0700

    Migrated to install-init -- works like a charm

--- grunt.js
+++ grunt.js
@@ -29,61 +29,13 @@ module.exports = function(grunt) {
       },
       globals: {}
     },
-    install: {
-      // 'pluginname': 'base directory'
+    'install-init': {
       'gruntplugin2': 'src'
     }
   });
 
-  // Create a helper to copy files from src to ~/.grunt/tasks/init/
-  // TODO: This should be its own repo as well
-  var path = require('path'),
-      rimraf = require('rimraf');
-  grunt.registerMultiTask('install', 'Helper to copy files to ~/.grunt', function () {
-    var file = this.file,
-        baseDir = file.src,
-        pluginName = file.dest,
-        srcJS = baseDir + '/' + pluginName + '.js',
-        src = baseDir + '/' + pluginName + '/**',
-        dest = grunt.file.userDir('tasks/init');
-
-    // Grab the srcDirs and srcFiles
-    var srcDirs = grunt.file.expandDirs({'dot': true}, src),
-        srcFiles = grunt.file.expandFiles({'dot': true}, src);
-
-    // Add srcJS to the head of the srcFiles
-    srcFiles.unshift(srcJS);
-
-    // Empty each of the srcDirs
-    srcDirs.forEach(function (srcDir) {
-      // Remove baseDir from srcDir
-      srcDir = srcDir.replace(baseDir, '');
-
-      // Empty the directory
-      var destDir = path.join(dest, srcDir);
-      rimraf.sync(destDir);
-    });
-
-    // Re-create each of the srcDirs
-    srcDirs.forEach(function (srcDir) {
-      // Remove baseDir from srcDir
-      srcDir = srcDir.replace(baseDir, '');
-
-      // Re-creaate the directory
-      var destDir = path.join(dest, srcDir);
-      grunt.file.mkdir(destDir);
-    });
-
-    // Copy over each of the srcFiles
-    srcFiles.forEach(function (srcFile) {
-      // Remove baseDir from srcFile
-      var _srcFile = srcFile.replace(baseDir, '');
-
-      // Copy over the file
-      var destFile = path.join(dest, _srcFile);
-      grunt.file.copy(srcFile, destFile);
-    });
-  });
+  // Load grunt-install-init
+  grunt.loadNpmTasks('grunt-install-init');
 
   // Default task.
   grunt.registerTask('default', 'lint test');
