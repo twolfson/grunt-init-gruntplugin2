a2096729efccdad173fd33c942192fe7a228e6de
Todd Wolfson <todd@twolfson.com>
2012-10-31 22:12:49 -0700

    Working through copying of files

--- grunt.js
+++ grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
       files: ['test/**/*.js']
     },
     lint: {
-      files: ['grunt.js', 'tasks/**/*.js', 'test/**/*.js']
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
     },
     watch: {
       files: '<config:lint.files>',
@@ -28,11 +28,38 @@ module.exports = function(grunt) {
         es5: true
       },
       globals: {}
+    },
+    install: {
+      gruntplugin2: {
+        'src': 'src'
+      }
     }
   });
 
-  // Load local tasks.
-  grunt.loadTasks('tasks');
+  // Create a helper to copy files from src to ~/.grunt/tasks/init/
+  // TODO: This should be its own repo as well
+  var platform = process.platform;
+  console.log(platform);
+  grunt.registerMultiTask('install', 'Helper to copy files to ~/.grunt', function () {
+    // TODO: Windows support
+    var file = this.file,
+        src = file.src,
+        dest = '~/.grunt/tasks/init/';
+
+    // Grab the srcDirs and srcFiles
+    var srcDirs = grunt.file.expandDirs(src),
+        srcFiles = grunt.file.expandFiles(src);
+
+    // Delete and re-create each of the srcDirs
+    srcDirs.forEach(function (srcDir) {
+      grunt.file.write(srcDir);
+    });
+
+    // Copy over each of the srcFiles
+    srcFiles.forEach(function (srcFile) {
+      grunt.file.write(srcFile);
+    });
+  });
 
   // Default task.
   grunt.registerTask('default', 'lint test');
